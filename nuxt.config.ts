8c6c0868b69f9db9562b5c6b029df2bbc68cc0e4
Ivan <ivangomezlapenya@gmail.com>
2025-11-07 12:29:35 +0100

    feat: add Storybook integration for component development

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,12 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@nuxt/eslint',
   ],
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
   typescript: {
     strict: true,
     shim: false,
